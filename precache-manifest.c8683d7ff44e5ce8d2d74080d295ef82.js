0d1a7dc6d3ca1525c10372424673cc19e0c1ed95
Patryk <patryk@Mac-mini-Patryk.local>
2020-09-20 20:10:39 +0200

    Updates

---
+++ precache-manifest.c8683d7ff44e5ce8d2d74080d295ef82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c74362f816171f785eaa393f77a0ad4",
+    "url": "/cp-messenger-clone/index.html"
+  },
+  {
+    "revision": "22ffe770552c54185e3c",
+    "url": "/cp-messenger-clone/static/css/main.16863eb3.chunk.css"
+  },
+  {
+    "revision": "855d0277c3c9cb604bcc",
+    "url": "/cp-messenger-clone/static/js/2.d56b0d01.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/cp-messenger-clone/static/js/2.d56b0d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ffe770552c54185e3c",
+    "url": "/cp-messenger-clone/static/js/main.146ac7bc.chunk.js"
+  },
+  {
+    "revision": "336c20f8733f80f706be",
+    "url": "/cp-messenger-clone/static/js/runtime-main.caff6a4b.js"
+  }
+]);
